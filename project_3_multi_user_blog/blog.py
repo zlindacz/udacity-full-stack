501d4cce3b7b19da5c1c8db9907735e21ad47fc3
Linda Zhang <zlindacz@gmail.com>
2017-02-10 01:14:58 -0800

    project 3 multi user blog, yaml config file

---
+++ project_3_multi_user_blog/blog.py
@@ -0,0 +1,21 @@
+#######################################################
+# Udacity Full Stack Web Developer Nanodegree
+# Project: Multi User Blog
+# Date Completed: 2/9/2017
+# Submitted by: Linda Zhang
+#######################################################
+
+#######################################################
+# The blog.py module contains the back end logic
+# for the blog, using the Google App Engine.
+# Docs: https://cloud.google.com/appengine/docs/python/
+# jinja 2 is the template engine used
+# Docs: http://jinja.pocoo.org/docs/2.9/
+# To view the blog during development,
+# run in terminal from outside directory:
+# `dev_appserver.py <dirname>` and access at
+# http://localhost:8080/
+# To deploy, run in terminal:
+# `gcloud app deploy <yaml file>`
+# Access at `unique-name.appspot.com/path`
+#######################################################
